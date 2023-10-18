4e03b1e92a31de25a68c0d8f56cd594a2f12cca6
micheal-el07 <mikhailkamal00@gmail.com>
2023-10-19 00:00:40 +0800

    connect to db trial

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from  flask import Flask, render_template, url_for
+from  flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# from app import db
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+# app.app_context()
+with app.app_context():
+    # Now you can perform operations that require the application context
+    db.create_all()  # Example operation
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return 'hello'
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
