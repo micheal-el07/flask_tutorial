484e2d0504bcc96012046bbeb8a041b784b37c9d
micheal-el07 <mikhailkamal00@gmail.com>
2023-10-23 22:44:43 +0800

    Issue to connect to db.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from  flask import Flask, render_template, url_for, request
+from  flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,14 +19,25 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr(self):
+    def __repr__(self):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return 'hello'
+        return 'hello world'
+        # task_content = request.form['content']
+        # new_task = Todo(content=task_content)
+
+        # try:
+        #     db.session.add(new_task)
+        #     db.session.commit()
+        #     return redirect('/')
+        # except:
+        #     return 'There was an issue adding your task!'
+
     else:
+        # taks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html')
 
 
