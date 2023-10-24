d4db4a4cdbe1aec83803c0b4564d5b98dc2d79b7
micheal-el07 <mikhailkamal00@gmail.com>
2023-10-24 23:01:20 +0800

    try to use postgresql for db

--- app.py
+++ app.py
@@ -6,35 +6,49 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+
+# db config for postgresql
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pasuwok@123@localhost/flask_intro'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 # app.app_context()
-with app.app_context():
-    # Now you can perform operations that require the application context
-    db.create_all()  # Example operation
+# with app.app_context():
+#     # Now you can perform operations that require the application context
+#     db.create_all()  # Example operation
 
 class Todo(db.Model):
+ #   __tablename__='to_do'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
+    
+    # def __init__(self,id,content,date_created):
+    #     self.id = id
+    #     self.content = content
+    #     self.date_created = date_created
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return 'hello world'
+        # return 'hello world'
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        # # using postgresql
         # task_content = request.form['content']
         # new_task = Todo(content=task_content)
 
-        # try:
-        #     db.session.add(new_task)
-        #     db.session.commit()
-        #     return redirect('/')
-        # except:
-        #     return 'There was an issue adding your task!'
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task!'
 
     else:
         # taks = Todo.query.order_by(Todo.date_created).all()
